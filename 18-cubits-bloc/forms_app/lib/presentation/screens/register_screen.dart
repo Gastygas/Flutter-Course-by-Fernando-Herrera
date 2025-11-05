5150e1ccc7bc48c4fc38d8dbb1cdee7d0e105523
Gastygas <gastonrodrigogonzalez55@gmail.com>
2025-11-05 10:30:05 -0300

    finish section 20

--- 18-cubits-bloc/forms_app/lib/presentation/screens/register_screen.dart
+++ 18-cubits-bloc/forms_app/lib/presentation/screens/register_screen.dart
@@ -49,7 +49,7 @@ class _RegisterForm extends StatefulWidget {
 }
 
 class _RegisterFormState extends State<_RegisterForm> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  // final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   // String username = '';
   // String email = '';
   // String password = '';
@@ -57,40 +57,39 @@ class _RegisterFormState extends State<_RegisterForm> {
   @override
   Widget build(BuildContext context) {
     final registerCubit = context.watch<RegisterCubit>();
+    final username = registerCubit.state.username;
+    final email = registerCubit.state.email;
+    final password = registerCubit.state.password;
 
     return Form(
-      key: _formKey,
+      // key: _formKey,
       child: Column(
         children: [
           CustomTextFormField(
             label: 'Username',
-            onChanged: (value) {
-              registerCubit.usernameChanged(value);
-              _formKey.currentState?.validate();
-            },
-            validatior: (value) {
-              if (value == null || value.isEmpty) return 'Field required';
-              if (value.trim().isEmpty) return 'Field required';
-              if (value.length < 4) return 'More than 4 characters please';
-              return null;
-            },
+            onChanged: registerCubit.usernameChanged,
+            errorMessage: username.errorMessage,
+            // validatior: (value) {
+            //   if (value == null || value.isEmpty) return 'Field required';
+            //   if (value.trim().isEmpty) return 'Field required';
+            //   if (value.length < 4) return 'More than 4 characters please';
+            //   return null;
+            // },
           ),
 
           const SizedBox(height: 20),
 
           CustomTextFormField(
             label: 'Email',
-            onChanged: (value) {
-              registerCubit.emailChanged(value);
-              _formKey.currentState?.validate();
-            },
-            validatior: (value) {
-              if (value == null || value.isEmpty) return 'Field required';
-              if (value.trim().isEmpty) return 'Field required';
-              final emailRegExp = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
-              if (!emailRegExp.hasMatch(value)) return 'Enter a valid email';
-              return null;
-            },
+            onChanged: registerCubit.emailChanged,
+            errorMessage: email.errorMessage,
+            // validatior: (value) {
+            //   if (value == null || value.isEmpty) return 'Field required';
+            //   if (value.trim().isEmpty) return 'Field required';
+            //   final emailRegExp = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+            //   if (!emailRegExp.hasMatch(value)) return 'Enter a valid email';
+            //   return null;
+            // },
           ),
 
           const SizedBox(height: 10),
@@ -98,25 +97,22 @@ class _RegisterFormState extends State<_RegisterForm> {
           CustomTextFormField(
             label: 'Password',
             obscure: true,
-            onChanged: (value) {
-              registerCubit.passwordChanged(value);
-              _formKey.currentState?.validate();
-            },
-            validatior: (value) {
-              if (value == null || value.isEmpty) return 'Field required';
-              if (value.trim().isEmpty) return 'Field required';
-              if (value.length < 4) return 'More than 6 characters please';
-              return null;
-            },
+            onChanged: registerCubit.passwordChanged,
+            errorMessage: password.errorMessage,
+            // validatior: (value) {
+            //   if (value == null || value.isEmpty) return 'Field required';
+            //   if (value.trim().isEmpty) return 'Field required';
+            //   if (value.length < 4) return 'More than 6 characters please';
+            //   return null;
+            // },
           ),
 
           const SizedBox(height: 10),
 
           FilledButton.tonalIcon(
             onPressed: () {
-              // if(_formKey.currentState == null) return;
-              final isValid = _formKey.currentState!.validate();
-              if (!isValid) return;
+              // final isValid = _formKey.currentState!.validate();
+              // if (!isValid) return;
 
               registerCubit.onSubmit();
             },
